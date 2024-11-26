8e5a4b013d92ec269b1fc74528ebc4a9aaee6582
QuangDinh2407 <lequangdinh0609@gmail.com>
2024-11-26 14:12:13 +0700

    update

--- src/main/java/com/sportshop/Controller/AdminController.java
+++ src/main/java/com/sportshop/Controller/AdminController.java
@@ -8,7 +8,6 @@ import com.sportshop.Service.UserService;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -17,18 +16,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Date;
-
-import static com.sportshop.Utils.ValidationUtil.*;
-
 @Controller
 @RequestMapping("/admin")
-public class adminController {
+public class AdminController {
 
     @Autowired
     UserService userService;
